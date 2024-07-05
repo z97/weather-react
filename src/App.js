7f26a81335df7ac6d33b3a2e87c59e885f4fa655
kliba <klibaner@mail.com>
2024-07-05 18:10:50 +0300

    hello3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
+import 'dotenv/config'
 
 // Importing images
 import sunny from './images/sunny.png';
